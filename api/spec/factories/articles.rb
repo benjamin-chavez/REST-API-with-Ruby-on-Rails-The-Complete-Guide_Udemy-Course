ee0c667294b2230897ea54d1e46b214d9f88d2ea
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-30 23:57:41 -0500

    added RSpec routing test for the /articles route

--- api/spec/factories/articles.rb
+++ api/spec/factories/articles.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :article do
     title { "Sample Article" }
     content { "Sample content" }
-    slug { "sample-article" }
+    # slug { "sample-article" }
+    sequence(:slug) { |n| "#{n}" }
   end
 end
