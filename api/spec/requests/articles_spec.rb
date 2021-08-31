7e09b2007ff526f337d12adf8b983df6be6dba3d
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-31 02:00:02 -0500

    Cleaned up the articles test code with support/rails_helper.rb and syntax tweaks in /requests/articles_spec.rb

--- api/spec/requests/articles_spec.rb
+++ api/spec/requests/articles_spec.rb
@@ -14,22 +14,34 @@ RSpec.describe ArticlesController do
     it 'returns a proper JSON' do 
       article = create :article
       get '/articles'
-      body = JSON.parse(response.body).deep_symbolize_keys
-      # pp body
-      expect(body).to eq(
-        data: [
-          {
-            id: article.id.to_s,
-            type: 'article',
-            # type: 'articles',
-            attributes: {
-              title: article.title,
-              content: article.content,
-              slug: article.slug
-            }
-          }
-        ]
-      )
+      # body = JSON.parse(response.body).deep_symbolize_keys
+      # # pp body
+      expect(json_data.length).to eq(1)
+      expected = json_data.first
+      aggregate_failures do
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('article')
+        expect(expected[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
+      # THE FOLLOWING BLOCK OF CODE WAS IMPROVED THROUGH THE AGGREGATE FAILURES BLOCK ABOVE
+      # expect(json_data).to eq(
+      #   [
+      #     {
+      #       id: article.id.to_s,
+      #       type: 'article',
+      #       # type: 'articles',
+      #       attributes: {
+      #         title: article.title,
+      #         content: article.content,
+      #         slug: article.slug
+      #       }
+      #     }
+      #   ]
+      # )
     end
   end
 end
