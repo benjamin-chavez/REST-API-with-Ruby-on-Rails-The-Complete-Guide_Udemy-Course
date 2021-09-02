4dd7d95474592ecc970fb4dbceb88bc2ac19db34
Ben Chavez <ben.m.chavez@gmail.com>
2021-09-02 00:16:59 -0500

    test pagination resources

--- api/spec/requests/articles_spec.rb
+++ api/spec/requests/articles_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe ArticlesController do
   describe '#index' do
     it 'returns a success response' do
       get '/articles'
-      # expect(response).to have_http_status(:ok)
-      expect(response).to have_http_status(200)
+      # expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
 
     it 'returns a proper JSON' do 
@@ -28,6 +28,7 @@ RSpec.describe ArticlesController do
         )
       end
     end
+
       # THE FOLLOWING BLOCK OF CODE WAS IMPROVED THROUGH THE AGGREGATE FAILURES BLOCK ABOVE
       # expect(json_data).to eq(
       #   [
@@ -50,6 +51,22 @@ RSpec.describe ArticlesController do
         get '/articles'
         ids = json_data.map { |item| item[:id].to_i }
         expect(ids).to(eq([recent_article.id, older_article.id]))
+    end
+
+    it 'paginates results' do
+      article1, aritlce2, aritcle3 = create_list(:article, 3)
+      get '/index', params: { page: { number: 2, size: 1 }}
+      expect(json_data.length).to eq(1)
+      expect(json_data.first[:id]).to eq(article2.id)
+    end
+
+    it 'contains pagination links in the response' do
+      article1, aritlce2, aritcle3 = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 }}
+      expect(json['links'].length).to eq(5)
+      expect(json['links'].keys).to contain_exactly(
+        'self', 'first', 'prev', 'next', 'last'
+      )
     end
   end
 end
